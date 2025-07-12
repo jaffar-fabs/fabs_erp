b36d1795ab674d66dbce002625cd114186d8cd6d
ferose06 <63855946+ferose06@users.noreply.github.com>
2025-07-12 13:51:34 +0530

    Update settings.py

--- erp/settings.py
+++ erp/settings.py
@@ -29,6 +29,17 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+# CSRF Trusted Origins - Add your domain/port combinations here
+CSRF_TRUSTED_ORIGINS = [
+    'http://103.14.120.216:30000',
+    'http://103.14.120.216:80',
+    'http://103.14.120.216',
+    'https://103.14.120.216:30000',
+    'https://103.14.120.216:80',
+    'https://103.14.120.216',
+]
+
+
 # Application definition
 
 INSTALLED_APPS = [
